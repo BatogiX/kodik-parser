c6b8680e5798c593a5ba128adee011f73fd15897
batosgi <novak34291@gmail.com>
2025-08-10 13:15:44 +0300

    bump to 0.1.1, added regex and implemented `get_domain` function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use regex::Regex;
+
+pub fn parse(url: &str) -> Result<(), regex::Error> {
+    let domain = get_domain(url)?;
+
+    // let (data, player_response) = parse_player();
+
+    Ok(())
+}
+
+fn get_domain(url: &str) -> Result<&str, regex::Error> {
+    let domain_regex: Regex =
+        Regex::new(r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]")
+            .expect("Pattern is valid");
+
+    if let Some(domain) = domain_regex.find(url) {
+        return Ok(domain.as_str());
+    }
+
+    Err(regex::Error::Syntax("No valid domain found".to_owned()))
+}
+
+fn parse_player() {
+
 }
 
 #[cfg(test)]
@@ -7,8 +29,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn domain_with_scheme() {
+        let url = "https://kodik.info/seria/1484069/6a2e103e9acf9829c6cba7e69555afb1/720p";
+        let result = get_domain(url);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), "kodik.info");
+    }
+
+    #[test]
+    fn domain_without_scheme() {
+        let url = "kodik.info/seria/1484069/6a2e103e9acf9829c6cba7e69555afb1/720p";
+        let result = get_domain(url);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), "kodik.info");
     }
 }
