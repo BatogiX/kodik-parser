33c8a5e6ef52a268945e03add7d2f31c80dc5233
batosgi <novak34291@gmail.com>
2025-08-11 21:07:00 +0300

    bump to 0.3.0, implemented functions `spoof_random_agent`, `get_api_endpoint`, `extract_player_url`, added tests and refactor

--- src/parser.rs
+++ src/parser.rs
@@ -1,54 +1,53 @@
-use std::{collections::HashMap, error::Error, sync::LazyLock};
+use std::{
+    collections::HashMap,
+    sync::{LazyLock, RwLock},
+};
 
 use crate::scraper;
+use base64::{Engine as _, engine::general_purpose};
 use regex::Regex;
 use reqwest::Client;
 
-pub async fn parse(client: &Client, url: &str) -> Result<(), regex::Error> {
+static API_ENDPOINT: RwLock<String> = RwLock::new(String::new());
+
+pub async fn parse(client: &Client, url: &str) -> Result<(), Box<dyn std::error::Error>> {
     let domain = get_domain(url)?;
 
-    // let (data, player_response) = parse_player();
+    let response_text = scraper::get_with_fake_agent(client, url).await?;
+    let video_info = extract_video_info(&response_text)?;
+
+    if API_ENDPOINT.read().unwrap().is_empty() {
+        let player_url = extract_player_url(domain, &response_text)?;
+        let player_response_text = scraper::get_with_fake_agent(client, &player_url).await?;
+        *API_ENDPOINT.write().unwrap() = get_api_endpoint(&player_response_text)?;
+    }
 
     Ok(())
 }
 
-fn get_domain(url: &str) -> Result<&str, regex::Error> {
+fn get_domain(url: &str) -> Result<&str, Box<dyn std::error::Error>> {
     static DOMAIN_REGEX: LazyLock<Regex> = LazyLock::new(|| {
         Regex::new(r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]").unwrap()
     });
 
-    if let Some(domain) = DOMAIN_REGEX.find(url) {
-        return Ok(domain.as_str());
-    }
+    let domain = DOMAIN_REGEX.find(url).ok_or("No valid domain found")?;
 
-    Err(regex::Error::Syntax("No valid domain found".to_owned()))
+    Ok(domain.as_str())
 }
 
-async fn parse_player(client: &Client, url: &str) -> Result<String, reqwest::Error> {
-    let response_text = scraper::get_with_fake_agent(client, url).await?;
-
-    let data = extract_video_info(&response_text);
-
-    Ok(response_text)
-}
-
-fn extract_video_info(response_text: &str) -> Result<HashMap<&'static str, &str>, regex::Error> {
+fn extract_video_info(response_text: &str) -> Result<HashMap<&'static str, &str>, Box<dyn std::error::Error>> {
     static TYPE_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"videoInfo\.type = '(.*?)';").unwrap());
     static HASH_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"videoInfo\.hash = '(.*?)';").unwrap());
     static ID_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"videoInfo\.id = '(.*?)';").unwrap());
 
-    let video_info_type = TYPE_REGEX.captures(response_text);
-    let video_info_hash = HASH_REGEX.captures(response_text);
-    let video_info_id = ID_REGEX.captures(response_text);
-
-    if video_info_type.is_none() || video_info_hash.is_none() || video_info_id.is_none() {
-        return Err(regex::Error::Syntax("videoInfo not found".to_owned()));
-    }
+    let video_info_type = TYPE_REGEX.captures(response_text).ok_or("videoInfo.type not found")?;
+    let video_info_hash = HASH_REGEX.captures(response_text).ok_or("videoInfo.hash not found")?;
+    let video_info_id = ID_REGEX.captures(response_text).ok_or("videoInfo.id not found")?;
 
     let mut video_info: HashMap<&'static str, &str> = HashMap::with_capacity(6);
-    video_info.insert("type", video_info_type.unwrap().get(1).unwrap().as_str());
-    video_info.insert("hash", video_info_hash.unwrap().get(1).unwrap().as_str());
-    video_info.insert("id", video_info_id.unwrap().get(1).unwrap().as_str());
+    video_info.insert("type", video_info_type.get(1).unwrap().as_str());
+    video_info.insert("hash", video_info_hash.get(1).unwrap().as_str());
+    video_info.insert("id", video_info_id.get(1).unwrap().as_str());
     video_info.insert("bad_user", "True");
     video_info.insert("info", "{}");
     video_info.insert("cdn_is_working", "True");
@@ -56,28 +55,52 @@ fn extract_video_info(response_text: &str) -> Result<HashMap<&'static str, &str>
     Ok(video_info)
 }
 
+fn get_api_endpoint(player_response_text: &str) -> Result<String, Box<dyn std::error::Error>> {
+    static ENDPOINT_REGEX: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r#"\$\.ajax\([^>]+,url:\s*atob\(["\']([\w=]+)["\']\)"#).unwrap());
+
+    let encoded_api_endpoint = ENDPOINT_REGEX
+        .captures(&player_response_text)
+        .ok_or("There is no api endpoint in player response")?
+        .get(1)
+        .unwrap()
+        .as_str();
+
+    let api_endpoint = general_purpose::STANDARD.decode(encoded_api_endpoint)?;
+
+    Ok(String::from_utf8(api_endpoint)?)
+}
+
+fn extract_player_url(domain: &str, response_text: &str) -> Result<String, Box<dyn std::error::Error>> {
+    static PLAYER_PATH_REGEX: LazyLock<Regex> = LazyLock::new(|| {
+        Regex::new(r#"<script\s*type="text/javascript"\s*src="/(assets/js/app\.player_single[^"]*)""#).unwrap()
+    });
+
+    let player_path = PLAYER_PATH_REGEX
+        .captures(response_text)
+        .ok_or("There is no player path in response text")?
+        .get(1)
+        .unwrap()
+        .as_str();
+
+    Ok(format!("https://{domain}/{player_path}"))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn domain_with_scheme() {
-        let url = "https://kodik.info/seria/1484069/6a2e103e9acf9829c6cba7e69555afb1/720p";
-        let result = get_domain(url);
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), "kodik.info");
+    fn test_get_domain() {
+        let url_with_scheme = "https://kodik.info/seria/1484069/6a2e103e9acf9829c6cba7e69555afb1/720p";
+        let url_without_scheme = "kodik.info/seria/1484069/6a2e103e9acf9829c6cba7e69555afb1/720p";
+
+        assert_eq!("kodik.info", get_domain(url_with_scheme).unwrap());
+        assert_eq!("kodik.info", get_domain(url_without_scheme).unwrap());
     }
 
     #[test]
-    fn domain_without_scheme() {
-        let url = "kodik.info/seria/1484069/6a2e103e9acf9829c6cba7e69555afb1/720p";
-        let result = get_domain(url);
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), "kodik.info");
-    }
-
-    #[test]
-    fn kodik_data() {
+    fn test_extract_video_info() {
         let mut expected_video_info = HashMap::with_capacity(6);
         expected_video_info.insert("type", "seria");
         expected_video_info.insert("hash", "6a2e103e9acf9829c6cba7e69555afb1");
@@ -96,5 +119,35 @@ mod tests {
         let video_info = extract_video_info(response_text).unwrap();
 
         assert_eq!(expected_video_info, video_info);
+    }
+
+    #[test]
+    fn test_get_player_url() {
+        let domain = "kodik.info";
+        let response_text = r#"
+  </script>
+
+  <link rel="stylesheet" href="/assets/css/app.player.ffc43caed0b4bc0a9f41f95c06cd8230d49aaf7188dbba5f0770513420541101.css">
+  <script type="text/javascript" src="/assets/js/app.player_single.0a909e421830a88800354716d562e21654500844d220805110c7cf2092d70b05.js"></script>
+</head>
+<body class=" ">
+  <div class="main-box">
+    <style>
+  .resume-button { color: rgba(255, 255, 255, 0.75); }
+  .resume-button:hover { background-color: #171717; }
+  .resume-button { border-radius: 3px; }
+  .active-player .resume-button { border-radius: 3px; }"#;
+
+        let player_url = extract_player_url(domain, response_text).unwrap();
+        assert_eq!(
+            "https://kodik.info/assets/js/app.player_single.0a909e421830a88800354716d562e21654500844d220805110c7cf2092d70b05.js",
+            player_url
+        );
+    }
+
+    #[test]
+    fn test_get_api_endpoint() {
+        let player_response_text = r#"==t.secret&&(e.secret=t.secret),userInfo&&"object"===_typeof(userInfo.info)&&(e.info=JSON.stringify(userInfo.info)),void 0!==window.advertTest&&(e.a_test=!0),!0===t.isUpdate&&(e.isUpdate=!0),$.ajax({type:"POST",url:atob("L2Z0b3I="),"#;
+        assert_eq!("/ftor", get_api_endpoint(player_response_text).unwrap());
     }
 }
--- src/scraper.rs
+++ src/scraper.rs
@@ -3,24 +3,26 @@ use std::sync::LazyLock;
 use reqwest::{Client, header::USER_AGENT};
 use ua_generator::{fastrand, ua};
 
-static AGENTS: LazyLock<&'static Vec<&'static str>> = LazyLock::new(ua::all_static_agents);
+fn spoof_random_agent() -> &'static str {
+    static AGENTS: LazyLock<&'static Vec<&'static str>> = LazyLock::new(ua::all_static_agents);
+    AGENTS[fastrand::usize(..AGENTS.len())]
+}
+
 
 pub async fn get_with_fake_agent(client: &Client, url: &str) -> Result<String, reqwest::Error> {
-    let agent = AGENTS[fastrand::usize(..AGENTS.len())];
+    let agent = spoof_random_agent();
 
-    let response_text = client
-        .get(url)
-        .header(USER_AGENT, agent)
-        .send()
-        .await?
-        .text()
-        .await?;
+    let response_text = client.get(url).header(USER_AGENT, agent).send().await?.text().await?;
 
     Ok(response_text)
 }
 
 pub async fn post_with_fake_user_agent(client: &Client, url: &str) {
-    // let agent = AGENTS[fastrand::usize(..AGENTS.len())];
+    let agent = spoof_random_agent();
+}
+
+pub async fn post_request_ti_kodik() {
+
 }
 
 #[cfg(test)]
@@ -28,10 +30,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn user_agent() {
-        let agent1 = AGENTS[fastrand::usize(..AGENTS.len())];
-        let agent2 = AGENTS[fastrand::usize(..AGENTS.len())];
-        assert_ne!(agent1, agent2);
+    fn random_agent_is_not_always_same() {
+        let a1 = spoof_random_agent();
+        let a2 = spoof_random_agent();
+        assert_ne!(a1, a2);
     }
 
     #[tokio::test]
