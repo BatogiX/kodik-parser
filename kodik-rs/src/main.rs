0c919016fff71990e40d9fea4793607e7bf26e25
batosgi <novak34291@gmail.com>
2026-05-09 13:41:00 +0300

    Add stdin as an argument source

--- kodik-rs/src/main.rs
+++ kodik-rs/src/main.rs
@@ -1,9 +1,26 @@
 use kodik_rs::run;
-use std::{env, process::ExitCode};
+use std::{
+    env,
+    io::{self, IsTerminal, Read},
+    process::ExitCode,
+};
 
 #[tokio::main]
 async fn main() -> ExitCode {
-    let args: Vec<String> = env::args().collect();
+    let stdin = if io::stdin().is_terminal() {
+        String::new()
+    } else {
+        let mut stdin = String::new();
+
+        if let Err(err) = io::stdin().read_to_string(&mut stdin) {
+            eprintln!("failed to read stdin: {err}");
+            return ExitCode::FAILURE;
+        }
+
+        stdin
+    };
+
+    let args: Vec<String> = env::args().chain(stdin.split_whitespace().map(str::to_owned)).collect();
 
     run(args).await
 }
